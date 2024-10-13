a9f7e402534cf2d84dadb31a160ce30cbda024b1
Pankaj Debnath <pnkzdn580@gmail.com>
2024-10-13 09:52:37 +0530

    Push pankaj

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import CommonButton from "./CommonButton";
+import CommonInput from "./CommonInput";
+import CommonPostCard from "./CommonPostCard";
+import CommonSelect from "./CommonSelect";
 
-export { Header, Footer, Container, Logo, LogoutBtn}
+export { Header, Footer, Container, Logo, LogoutBtn, CommonButton, CommonInput, CommonPostCard, CommonSelect };
